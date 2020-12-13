2f58a1cccd6e2e7e64acf111e1b97445d14fdf59
liamjsc <ljschauerman@gmail.com>
2020-12-13 00:25:05 -0800

    styling topnav

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,23 +1,43 @@
 import { connect } from 'react-redux';
 import './TopNav.scss';
+
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import Logo from '../Logo/Logo.js';
 
 function TopNav(props = {}) {
-  const {} = props;
+  const {
+    username = 'liamjsc',
+  } = props;
   return (
     <div className="TopNav">
       <div className="site-title-nav">
         <Logo className="site-title" />
         <div className="nav">
-          <div className="nav-tab">Rank</div>
-          <div className="nav-tab">Favorites</div>
-          <div className="nav-tab">Create</div>
+          <div className="nav-tab">
+            <Button color="inherit">Rank</Button>
+          </div>
+          <div className="nav-tab">
+            <Button color="inherit">Favorites</Button>
+          </div>
+          <div className="nav-tab">
+            <Button color="inherit">Community</Button>
+          </div>
+          <div className="nav-tab">
+            <Button color="inherit">Create</Button>
+          </div>
         </div>
-        <div className="site-buttons">
-          {/* <Search/> */}
-          {/* <NotificationCenter/> */}
-          {/* <AccountCenter/> */}
+      </div>
+      <div className="site-buttons">
+        <div className="site-button">
+          <SearchIcon />
         </div>
+        <div className="site-button">
+          <NotificationsNoneIcon />
+        </div>
+        <div className="site-button">{username}</div>
+        {/* <AccountCenter/> */}
       </div>
     </div>
   );
